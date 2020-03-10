8264090cb2fb8b26ca2aebc4afc01c4d87feb272
Rafael Marmol <rafael.marmol1@marist.edu>
2020-03-10 18:48:18 -0400

    Able to encode and decode based on two input files

--- Driver_dnasteg.java
+++ Driver_dnasteg.java
@@ -3,13 +3,15 @@
  * Filename: Driver_dnasteg
  * Student name: Rafael Marmol
  *
- * Runs DNA steg 
+ * Encodes binary text file into comma separated values based on a given DNA reference strand
+ * Decodes comma separated values against given DNA reference strand into binary text
  *
- * Class: Driver_grephy
+ * Class: Driver_steg
  */
  import java.io.*;
  import java.util.Scanner;
  import java.util.ArrayList;
+ import java.util.Arrays;
  
 class Driver_dnasteg{
   
@@ -26,97 +28,91 @@ class Driver_dnasteg{
     String inputFile = "";
     String dnaRefFile = "";
     boolean eArg = false;
-    booelean dArg = false;
+    boolean dArg = false;
       
     // Set arguments from cmd line to variables
-    if(args.length == 0){
-      System.out.println("Please enter input file");
+    //encrypt arg
+    if(args[0].equals("-e")){
+      eArg = true;
+      inputFile = args[1];
+      dnaRefFile = args[2];      
+        
+    }
+    //decrypt arg
+    else if(args[0].equals("-d")){
+      dArg = true;
+      inputFile = args[1];
+      dnaRefFile = args[2];
     }
     else {
-      inputFile = args[0];
+      System.out.println("Invalid arguments.");
     }
-    //dnaRefFile = args[1];
 
       
     // work with desired file if found
     File fileIn = new File (inputFile);
-    File fileSeq = new File (dnaRefFile);
+    File fileRef = new File (dnaRefFile);
      
     try{
        
-      // scanners for checking alphabet and another to check each line 
+      // scanners to check each line of input file
       Scanner inputDecode = new Scanner(fileIn);
-      //Scanner inputRef = new Scanner(dnaRefFile);
+      Scanner inputRef = new Scanner(fileRef);
         
-      // create array to hold alphabet of file (max value of # accepted characters (A-Z, a-z, 0-9))
+      // create array to hold binary numbers
       ArrayList<String> dnaToDecode = new ArrayList<String>();
+      ArrayList<String> binToEncode = new ArrayList<String>();
       
      
-      
-      // used to keep place in array
-      int count = 0;
-      
-      //used get each letter into a string
-      String fileString = "";
+      //used to build string
       String temp = "";
      
-      while(inputDecode.hasNext()){
-
-        temp+=inputDecode.next();
+      //if encoding grabs 2 bits at a time and adds to arraylist
+      if(eArg == true){
         
-      }
-      
-      
-      //put 2 letters
-      // Creating array of string length 
-        //char[] dnaCode = new char[temp.length()]; 
-  
-        // Copy character by character into array 
+        while(inputDecode.hasNext()){
+
+          temp+=inputDecode.next();
+        
+        }
+        
+        
         for (int i = 0; i < temp.length(); i++) { 
-            if(i == temp.length()-1){
-              dnaToDecode.add(temp.substring(i, temp.length()));
-            }
-            else{
-              dnaToDecode.add(temp.substring(i,i+2));
-              i++;
-            }
-        } 
-  
-        
-      //print contents of arraylist
-      /*for(String s:dnaToDecode){
-        System.out.println(s);
-      }*/
-      
-      
-      
-      //convert binary to DNA
-      dnaToDecode = convertToDNA(dnaToDecode);
-      /*for(String s:dnaToDecode){
-        System.out.print(s);
-      }*/
-      
-      //System.out.println("test:" + dnaToDecode.indexOf("A"));
-      //System.out.println("");
-      
-      //convert to complement
-      //((AC) (CG) (GT) (TA)
-      ArrayList<String> dnaComplement = new ArrayList<String>();
-      dnaComplement = getComplement(dnaToDecode);
-      /*for(String s:dnaComplement){
-        System.out.print(s);
+          if(i == temp.length()-1){
+            binToEncode.add(temp.substring(i, temp.length()));
+          }
+          else{
+            binToEncode.add(temp.substring(i,i+2));
+            i++;
+          }
+        }
       }
-      System.out.println("");*/
-      //Make pairs
+      //if decoding grabs text from file as string
+      else if (dArg == true){
+        
+        while(inputDecode.hasNext()){
+          temp += inputDecode.next();
+          
+        }
+        
+        //splits numbers
+        dnaToDecode = new ArrayList<String>(Arrays.asList(temp.split(",")));
+        
+      }
       
+      
+      //get reference strand
+      String dnaRef = "";
 
-      //find indexes in dna reference strand
-      String dnaRef = "AGCACTCTCAAAGATAGAGGGCGTTGTCAAGAATAAGACCGGATCTCTCGCCAACCACCCACCCTTTCATGACTTAGCTTAGTGCCTCAACATTGTGCCACAGGCACCGGGTAGAGCCCAGACAGTTAAGTAGTGATGCTGTGCTACAAATAGCTGGATATTTTTTTAGGACGGTTGCACTTACACTATCCGTGGTTGGCGGAGATCAACTGCAGCAGAACGCGGCAGCGTGGTATTTCTGTTACTGACCCCAGTAGTTGAGGCAGCTTGTATGTGAACGCAATCGAACGCACTATAGGAAACCCTTGGGTACGTAGGTCCTTTAACACAATTAGTGAGTGTAAAGAGTTGATTCCGTAAACCTCGTATATCTACTTCTGTAGGCCATCCGGGAGTACACTAAGTTCCAGTCATCCGTGAACTGGTGGTGTGCCCAGGCTTTCGATACTTGCTGCTTGGCAGAGCGTCATGCTTCGAGCGGCGATTATGTACGGAAATAAATAATACTTGCGGTGTTGCGGTTCCGTCGCCATAGGTTCAGACTTGGCCTATGCTCTGGCATCGCTACGTAAGCGGCGCGTTGTATACGGCGAGCGGTGGTCTACGGCGTACGGTGTTAACCACCGACTGGTAAGAGAGGGTTCGTGCAGGTTTTTGGTGGGAAGTTCGTAGCTTTTGGTATTCAGACCTGGAATATCCGCGGTACAATGCTGCACAAGTGTATGGACAGTGTTACCGGCTTATCCCTATAGCAACTTTCGTAATATACCTGGGGAGCCCCGGCGACGTCGTGTATACCGCTCCCGCTATGGCATTCCTCCAAACATGTGTCTAATCTATCGGCGGATCGCCCACAGCAAACGAGTTACGAAGAATATAACTTCGATATATAGGAGTAAGTATTCTAGGACTCTTCTGAACGCACTTAGAACAGGTTAGCTCTTTGATACACCATATTGTTGATTTTGTATTGCTTGGGCGGCACCCCAGGCGAAGCTAT";
+      while(inputRef.hasNext()){
 
-      //System.out.println(dnaRef);
+        dnaRef+=inputRef.next();
+        
+      }     
+      
       //convert dnaRef to array with 2 letters
-      ArrayList<String> dnaRefList = new ArrayList<String>();
-      for (int i = 0; i < dnaRef.length(); i++) { 
+        ArrayList<String> dnaRefList = new ArrayList<String>();
+        for (int i = 0; i < dnaRef.length(); i++) { 
             if(i == dnaRef.length()-3){
               dnaRefList.add(dnaRef.substring(i, dnaRef.length()));
               i = dnaRef.length();
@@ -125,48 +121,88 @@ class Driver_dnasteg{
               dnaRefList.add(dnaRef.substring(i,i+2));
               i++;
             }
-      }
-      
-      /*System.out.println("Reference list:");
-      for(String s:dnaRefList){
-        System.out.println(s);
-      }
-      System.out.println("");*/
-      
-      
-      //convert dnaComplement to pairs
-      ArrayList<String> dnaPairs = new ArrayList<String>();
-      for(int i = 0; i < dnaComplement.size(); i++){
-        if(i == dnaComplement.size()-2){
-          dnaPairs.add(dnaComplement.get(i)+dnaComplement.get(dnaComplement.size()-1));
-          i = dnaComplement.size();
         }
-        else{
-          dnaPairs.add(dnaComplement.get(i)+dnaComplement.get(i+1));
-          i++;
+        
+      if (eArg == true){
+        //convert binary to DNA
+        binToEncode = convertToDNA(binToEncode);
+      
+      
+        //convert to complement
+        //((AC) (CG) (GT) (TA)
+        ArrayList<String> dnaComplement = new ArrayList<String>();
+        dnaComplement = getComplement(binToEncode);
+      
+        //convert dnaComplement to pairs
+        ArrayList<String> dnaPairs = new ArrayList<String>();
+        for(int i = 0; i < dnaComplement.size(); i++){
+          if(i == dnaComplement.size()-2){
+            dnaPairs.add(dnaComplement.get(i)+dnaComplement.get(dnaComplement.size()-1));
+            i = dnaComplement.size();
+          }
+          else{
+            dnaPairs.add(dnaComplement.get(i)+dnaComplement.get(i+1));
+            i++;
+          }
+        
+        }
+      
+        //find indexes
+        ArrayList<String> indexes = new ArrayList<String>();
+        int n = 0;
+        for(String s:dnaPairs){
+          n = dnaRefList.indexOf(s);
+          indexes.add(String.valueOf(n));
+        }
+      
+        //printing index values of reference strand
+        for(String s:indexes){
+          System.out.print(s + ", ");
+        }      
+      }
+      
+      //decode process
+      else if (dArg == true){
+        
+        //start with getting index values
+        //find indexes
+        ArrayList<String> indexValues = new ArrayList<String>();
+        String refVal = "";
+        for(String s:dnaToDecode){
+          refVal = dnaRefList.get(Integer.valueOf(s));
+          indexValues.add(refVal);
+        }
+        
+        //convert to singles
+        ArrayList<String> dnaSingles = new ArrayList<String>();
+        for(String s:indexValues){
+          dnaSingles.add(s.substring(0,1));
+          dnaSingles.add(s.substring(1,s.length()));
+        }
+        
+        //convert index values to complement
+        dnaSingles = getComplement(dnaSingles);
+        
+        //convert to binary
+        dnaSingles = convertToBin(dnaSingles);
+        
+        
+        //print binary
+        for(int i = 0; i < dnaSingles.size(); i++){
+          if(((i) % 4 ) == 0 && i != 0){
+            System.out.print(" " + dnaSingles.get(i));
+          }
+          else{
+            System.out.print(dnaSingles.get(i));
+          }
         }
         
       }
       
-      
-      //find indexes
-      ArrayList<String> indexes = new ArrayList<String>();
-      int n = 0;
-      for(String s:dnaPairs){
-        n = dnaRefList.indexOf(s);
-        indexes.add(String.valueOf(n));
-      }
-      /*for(String s:dnaPairs){
-        System.out.println(s);
-      }
-      
-      System.out.println("");*/
-
-      for(String s:indexes){
-        System.out.print(s + ", ");
-      }      
-      
-      // print new value
+      //if not true
+      else{
+        System.out.println("Something went wrong");
+      }  
       
     }
     
@@ -199,6 +235,28 @@ class Driver_dnasteg{
     
   }
   
+  public static ArrayList<String> convertToBin (ArrayList<String> s){
+    ArrayList<String> bin = new ArrayList<String>();
+    
+    for(int i = 0; i < s.size(); i++){
+      if(s.get(i).equals("A")){
+        bin.add("00");
+      }
+      else if (s.get(i).equals("T")){
+        bin.add("01");
+      }
+      else if (s.get(i).equals("C")){
+        bin.add("10");
+      }
+      else if (s.get(i).equals("G")){
+        bin.add("11");
+      }
+    }
+    
+    return bin;
+    
+  }
+  
   public static ArrayList<String> getComplement (ArrayList<String> s){
     ArrayList<String> comp = new ArrayList<String>();
     
@@ -219,5 +277,7 @@ class Driver_dnasteg{
       
       return comp;
   }
+  
+  
 
  }
